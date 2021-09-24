89311841bc509e7e2fa816d465bcb4294e26bae8
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-23 23:02:48 -0300

    Salvando tarefa no dia e turno selecionado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 let box = document.querySelectorAll('.box');
+console.log(box);
+
+for (let index = 0; index < box.length; index++) {
+  box[index].addEventListener('click', (event) => {
+    console.log(event.target);
+    let taskPrompt = prompt('Digite a tarefa');
+    let task = document.createElement('p');
+    task.innerText = taskPrompt;
+    box[index].append(task);
+  });
+}
